b361128040f6392d473d200160662b4fb8e71c2f
Wolfgang Goedel <wgoedel@gmail.com>
2017-03-19 16:13:13 +0100

    step-01 - first components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const MESSAGE = 'React-200 @ SFEIR';
+
+const App = () => (
+  <div className="App">
+    <header>
+      insérez votre AppBar ici
+    </header>
+    <main>
+      insérez votre Card ici
+      et passez "{MESSAGE}" comme prop
+    </main>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'materialize-css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
