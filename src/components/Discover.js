08e4e21e58b7bbff332a47336b6f4366671b0224
Wolfgang Goedel <wgoedel@gmail.com>
2017-03-24 10:04:17 +0100

    step-03 - solution

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PEOPLE from '../data/people.json';
 
 import Person from './Person';
 
-const randomPerson = PEOPLE[Math.floor(Math.random() * PEOPLE.length)];
+class Discover extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: PEOPLE,
+      current: 0
+    }
+  }
 
-const Discover = () => (
-  <Person {...randomPerson} />
-);
+  componentDidMount() {
+    this.interval = setInterval(() => this.nextPerson(), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  nextPerson() {
+    const { current, people } = this.state;
+    const next = (current + 1) % people.length;
+    this.setState({
+      current: next
+    });
+  }
+  
+  render() {
+    const { people, current } = this.state;
+    return (
+      <Person {...people[current]} />
+    );
+  }
+}
 
 export default Discover;
