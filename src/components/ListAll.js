03859e7f03e0cc0eeec7d74997e29769dc9e921a
Wolfgang Goedel <wgoedel@gmail.com>
2017-03-27 12:42:09 +0200

    step-07 - solution

--- src/components/ListAll.js
+++ src/components/ListAll.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person';
+import SearchInput from './SearchInput';
 
-const ListAll = ({ people }) => (
-  <div className="card-container">
-    { people.map(person => 
-      <Person {...person} key={person.id} />
-    )}
-  </div>
-);
+// utils
+
+const filterPerson = search => person => {
+  if (!search) {
+    return true;
+  } else {
+    const re = new RegExp(search, 'i');
+    return re.test(person.firstname) || re.test(person.lastname);
+  }
+};
+
+// state management
+
+const searchChanged = value => ({ search }) => ({
+  search: value || ''
+});
+
+// Component
+
+class ListAll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+  }
+
+  searchChanged = event => {
+    this.setState(searchChanged(event.target.value));
+  }
+  
+  render() {
+    const { people } = this.props;
+    const { search } = this.state;
+    return (
+      <div className="ListAll">
+        <div className="card-container">
+          { people
+            .filter(filterPerson(search))
+            .map(person => 
+              <Person {...person} key={person.id} />
+            )
+          }
+        </div>
+        <div className="control-container">
+          <SearchInput id="search" label="search by name"
+            value={search}
+            onChange={this.searchChanged}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ListAll;
