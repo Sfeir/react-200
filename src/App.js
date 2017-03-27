81fae28ec1b964246a4b2494c028429a241ab2fa
Wolfgang Goedel <wgoedel@gmail.com>
2017-03-27 13:39:28 +0200

    step-08 - solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 import AppBar from './components/AppBar';
@@ -6,17 +7,12 @@ import Discover from './components/Discover';
 import ListAll from './components/ListAll';
 import Spinner from './components/Spinner';
 
-const DISCOVER = 'discover';
-const LISTALL = 'show all';
-
-const other = shown => shown === DISCOVER ? LISTALL : DISCOVER;
-const toggleShown = ({ shown }) => ({ shown: other(shown) });
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      shown: LISTALL,
+      // shown: LISTALL,
       people: null
     };
   }
@@ -26,22 +22,26 @@ class App extends Component {
     .then(res => res.json())
     .then(people => this.setState({ people }));
   }
-
-  toggleShown = () => this.setState(toggleShown);
   
   render() {
-    const { shown, people } = this.state;
+    const { people } = this.state;
     return (
       <div className="App">
         <header>
-          <AppBar show={other(shown)} toggleShow={this.toggleShown}/>
+          <AppBar />
         </header>
         <main>
           { people === null
           ? <Spinner />
-          : shown === LISTALL
-          ? <ListAll people={people} />
-          : <Discover people={people} />
+          : <Switch>
+              <Route path="/all" render={() =>
+                <ListAll people={people} />
+              } />
+              <Route path="/discover" render={() =>
+                <Discover people={people} />
+              } />
+              <Redirect to="/all" />
+            </Switch>
           }
         </main>
       </div>
