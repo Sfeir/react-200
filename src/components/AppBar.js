f64c1324a9e6e31efae8e9f5455f55e7e16c886d
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-16 16:47:07 +0200

    fix appBar link warning

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,7 +6,7 @@ const AppBar = ({show, toggleShow}) => (
   <nav className="AppBar">
     <img className="AppBar-logo" src={logo} aria-label="people" alt="People" />
     <hr/>
-    <a href="#" onClick={toggleShow}>{show}</a>
+    <a onClick={toggleShow}>{show}</a>
   </nav>
 );
 
