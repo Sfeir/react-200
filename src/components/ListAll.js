388e7a27dbf012ea680a9ffc055f968fa3a9215a
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-16 17:21:19 +0200

    add control-container to help with layout - ready for exo7

--- src/components/ListAll.js
+++ src/components/ListAll.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Person from './Person';
 
 const ListAll = ({ people }) => (
-  <div className="card-container">
-    { people.map(person => 
-      <Person person={person} key={person.id} />
-    )}
-  </div>
+  <div>
+    <div className="card-container">
+      { people.map(person => 
+        <Person person={person} key={person.id} />
+      )}
+    </div>
+    <div className="control-container">
+      place SearchInput here
+    </div>
+  </div>  
 );
 
 export default ListAll;
