6bdd3ee04dc743cc31706db445852f03d5257ca3
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-17 17:09:11 +0200

    add store Provider and configure store with devtools

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import 'materialize-css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Formsy from 'formsy-react';
 
+import { configureStore } from './store';
 import { setupVirtualServer } from './setup';
 import App from './App';
 import './index.css';
 
 const startApp = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
   );
 };
