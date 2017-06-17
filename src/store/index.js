7ff48d9a26c2087c938d0ae89a1fbb81715ce375
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-17 21:32:18 +0200

    configure redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const configureStore = () => createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export { peopleReceived, personReceived, searchChanged } from './actions';
