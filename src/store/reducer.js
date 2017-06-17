6b2b7d23f6d956375f9adcd91cd7b0293328ffb1
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-17 16:47:01 +0200

    implement reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,20 @@
-const initialState = {};
+import { replaceOrPrepend } from '../utils';
 
-const reducer = (state = initialState, action) => state;
+const initialState = {
+  people: []
+};
+
+const merge = replaceOrPrepend((a, b) => a.id === b.id);
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PEOPLE_RECEIVED':
+      return { people: action.people };
+    case 'PERSON_RECEIVED':
+      return { people: merge(action.person, state.people) };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
