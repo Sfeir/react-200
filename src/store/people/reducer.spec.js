156360540ae28c146701d8934461c3fb1e7662c8
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-20 20:19:51 +0200

    tests for map/all people reducers - failing

--- src/store/people/reducer.spec.js
+++ src/store/people/reducer.spec.js
@@ -1,37 +1,67 @@
 import { peopleReceived, personReceived } from './actions';
 import { freeze } from '../../utils';
-import reducer from './reducer';
+import reducer, { mapReducer, allReducer } from './reducer';
+
+const testPeople = freeze([
+  { id: '1', firstname: 'John' },
+  { id: '2', firstname: 'Jane' },
+  { id: '3', firstname: 'Joe' }
+]);
 
 describe('peopleReducer', () => {
-  const testPeople = freeze([
-    { id: '1', firstname: 'John' },
-    { id: '2', firstname: 'Jane' },
-    { id: '3', firstname: 'Joe' }
-  ]);
+  it('should init to an object with a map and an all key', () => {
+    const actual = reducer(undefined, {});
+    expect(actual).toEqual({
+      map: {},
+      all: []
+    });
+  });
+});
 
-  const emptyState = freeze([]);
-
-  it('should initialize state with an empty array', () => {
-    const actualState = reducer(undefined, {});
-    expect(actualState).toEqual(emptyState);
+describe('peopleMapReducer', () => {
+  const testMap = freeze({
+    '1': { id: '1', firstname: 'John' },
+    '2': { id: '2', firstname: 'Jane' },
+    '3': { id: '3', firstname: 'Joe' }
   });
 
-  it('should set people array on PEOPLE_RECEIVED', () => {
+  it('should build a map of persons with their id as key on PEOPLE_RECEIVED', () => {
     const action = peopleReceived(testPeople);
-    const actualState = reducer(emptyState, action);
-    expect(actualState).toEqual(testPeople);
+    const actualState = mapReducer({}, action);
+    expect(actualState).toEqual(testMap);
   });
 
   it('should replace the received person on PERSON_RECEIVED if it already exists', () => {
     const action = personReceived({ id: '2', firstname: 'Jill' });
-    const actualState = reducer(testPeople, action);
-    const [first, , third] = testPeople;
-    expect(actualState).toEqual([first, action.person, third]);
+    const actualState = mapReducer(testMap, action);
+    expect(actualState).toEqual({...testMap, '2': action.person});
   });
 
-  it('should prepend the received person on PERSON_RECEIVED when it does not exist', () => {
+  it('should add the received person on PERSON_RECEIVED when it does not exist', () => {
     const action = personReceived({ id: '4', firstname: 'Jill' });
-    const actualState = reducer(testPeople, action);
-    expect(actualState).toEqual([action.person].concat(testPeople));
+    const actualState = mapReducer(testMap, action);
+    expect(actualState).toEqual({...testMap, '4': action.person});
+  });
+});
+
+describe('peopleAllReducer', () => {
+  const testAll = freeze(['1', '2', '3']);
+
+  it('should build the array of ids on PEOPLE_RECEIVED', () => {
+    const action = peopleReceived(testPeople);
+    const actualState = allReducer([], action);
+    expect(actualState).toEqual(testAll);
+  });
+
+  it('should do nothing on PERSON_RECEIVED if it already exists', () => {
+    const action = personReceived({ id: '2', firstname: 'Jill' });
+    const actualState = allReducer(testAll, action);
+    expect(actualState).toBe(testAll);
+  });
+
+  it('should prepend the received person id on PERSON_RECEIVED when it does not exist', () => {
+    const action = personReceived({ id: '4', firstname: 'Jill' });
+    const actualState = allReducer(testAll, action);
+    expect(actualState).toEqual(['4', ...testAll]);
   });
 });
