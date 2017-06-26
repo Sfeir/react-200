668ba5d16c4c6946167cdafde541bf21c050754e
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-26 12:42:32 +0200

    implement Discover as class with state

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 import Person from './Person';
 
-const Discover = ({ people }) => (
-  <Person person={people[Math.floor(Math.random() * people.length)]} />
-);
+const setNext = ({ current }, { people }) => ({
+  current: (current + 1) % people.length
+});
+
+class Discover extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0
+    }
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.setState(setNext), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  
+  render() {
+    const { people } = this.props;
+    const { current } = this.state;
+    return (
+      <Person person={people[current]} />
+    );
+  }
+}
 
 export default Discover;
