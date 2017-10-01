9fb2df260015cffd448e1e1ff38e02f5b067100f
Wolfgang Goedel <wgoedel@gmail.com>
2017-10-01 12:22:04 +0200

    upgrade to React 16

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const getEnhancer = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return composeEnhancers(applyMiddleware(thunk));
+}
 
 export const configureStore = () => createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  getEnhancer()
 );
 
 export {
