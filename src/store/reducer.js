3d7f5646c36d4d1a4242582830e854c3fea0b940
Wolfgang Goedel <wgoedel@gmail.com>
2017-10-01 14:48:55 +0200

    simplify ex (a little bit ;)

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,15 @@
-import { replaceOrPrepend } from '../utils';
+import { replaceOrPrependById } from '../utils';
 
 const initialState = {
   people: []
 };
-
-const merge = replaceOrPrepend((a, b) => a.id === b.id);
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'PEOPLE_RECEIVED':
       return { people: action.people };
     case 'PERSON_RECEIVED':
-      return { people: merge(action.person, state.people) };
+      return { people: replaceOrPrependById(action.person, state.people) };
     default:
       return state;
   }
