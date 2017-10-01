d845ad23b8349e1d320c7a3518c7ff4faa6460bb
Wolfgang Goedel <wgoedel@gmail.com>
2017-10-01 13:06:15 +0200

    configure Router a little bit differently

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'materialize-css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { setupVirtualServer } from './setup';
 import App from './App';
@@ -10,9 +10,9 @@ import './index.css';
 
 const startApp = () => {
   ReactDOM.render(
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>,
+    </Router>,
     document.getElementById('root')
   );
 };
