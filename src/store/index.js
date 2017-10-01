6f33cb830878ee9a19896c3ac2f51032ff246027
Wolfgang Goedel <wgoedel@gmail.com>
2017-10-01 12:22:04 +0200

    upgrade to React 16

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
 import rootReducer from './reducer';
 
+const getEnhancer = () => {
+  return window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
 export const configureStore = () => createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  getEnhancer()
 );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,18 +1,16 @@
-import { replaceOrPrepend } from '../utils';
+import { replaceOrPrependById } from '../utils';
 
 const initialState = {
   people: [],
   search: ''
 };
 
-const merge = replaceOrPrepend((a, b) => a.id === b.id);
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'PEOPLE_RECEIVED':
       return { ...state, people: action.people };
     case 'PERSON_RECEIVED':
-      return { ...state, people: merge(action.person, state.people) };
+      return { ...state, people: replaceOrPrependById(action.person, state.people) };
     case 'SEARCH_CHANGED':
       return { ...state, search: action.search };
     default:
