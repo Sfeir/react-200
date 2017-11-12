ac39d78f1838410ae1ed03ab75cca511657340ff
Wolfgang Goedel <wgoedel@gmail.com>
2017-11-12 12:07:48 +0100

    remove SW, replace with json-server
    and change these photos I've seen too much ;)

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { setupVirtualServer } from './setup';
 import App from './App';
 import './index.css';
 
-const startApp = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
-};
-
-setupVirtualServer()
-.then(startApp)
-.catch(console.error);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
