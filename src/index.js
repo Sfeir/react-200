b242c953112c90aebf6cb57b81f269170625e75d
Wolfgang Goedel <wgoedel@gmail.com>
2017-11-12 12:07:48 +0100

    remove SW, replace with json-server
    and change these photos I've seen too much ;)

--- src/index.js
+++ src/index.js
@@ -7,24 +7,17 @@ import { Provider } from 'react-redux';
 import Formsy from 'formsy-react';
 
 import { configureStore } from './store';
-import { setupVirtualServer } from './setup';
 import App from './App.container';
 import './index.css';
 
-const startApp = () => {
-  ReactDOM.render(
-    <Provider store={configureStore()}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
-  );
-};
-
-setupVirtualServer()
-.then(startApp)
-.catch(console.error);
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 Formsy.addValidationRule('isFrenchPhoneNumber', (_, value) => (
   /^0[0-9]{9}$/.test(value)
