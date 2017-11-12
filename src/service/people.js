4ae2bc7cce2126041ed3646514a1a194b7c2d48a
Wolfgang Goedel <wgoedel@gmail.com>
2017-11-12 12:07:48 +0100

    remove SW, replace with json-server
    and change these photos I've seen too much ;)

--- src/service/people.js
+++ src/service/people.js
@@ -3,17 +3,11 @@ export async function fetchPeople() {
   return await response.json();
 }
 
-export async function loadPerson(id) {
-  const response = await fetch(`/api/people/${id}`);
-  return await response.json();
-}
-
 export async function updatePerson(id, patch) {
-  const response = await fetch(`api/people/${id}`, {
+  const response = await fetch(`/api/people/${id}`, {
     method: 'PATCH',
     body: JSON.stringify(patch),
     headers: {'Content-Type': 'application/json'}
   });
-  if (response.status !== 204) throw response.statusText;
-  return await loadPerson(id);
+  return await response.json();
 }
