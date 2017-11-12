83f393966d928498c669fca49fa5ea6e839ba598
Wolfgang Goedel <wgoedel@gmail.com>
2017-11-12 12:07:48 +0100

    remove SW, replace with json-server
    and change these photos I've seen too much ;)

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import 'materialize-css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { setupVirtualServer } from './setup';
 import App from './App';
 import './index.css';
 
-const startApp = () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-};
-
-setupVirtualServer()
-.then(startApp)
-.catch(console.error);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
