a4e98ab4ba30ce765ba224a5c19be2b4dab849b8
Wolfgang Goedel <wgoedel@gmail.com>
2018-01-23 12:16:35 +0100

    upgrade packages

--- src/pages/ListAll.js
+++ src/pages/ListAll.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PersonCard from '../components/PersonCard';
 import SearchInput from '../components/SearchInput';
 
-
 const ListAll = ({ personIds, search, searchChanged }) => (
-  <div className="ListAll">
+  <Fragment>
     <div className="card-container">
       { personIds
         .map(personId => 
@@ -18,7 +17,7 @@ const ListAll = ({ personIds, search, searchChanged }) => (
         onChange={searchChanged}
       />
     </div>
-  </div>
+  </Fragment>
 );
 
 export default ListAll;
