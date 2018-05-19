b19651ca67fae90fe62dba70f8a16b0e00b3706c
Wolfgang Goedel <wgoedel@gmail.com>
2018-05-19 19:54:27 +0200

    prettier

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Card.css';
 
-const Card = (props) => (
+const Card = props => (
   <section className="Card card">
     <div className="card-content">
-      <div className="card-title">
-        {props.title}
-      </div>
+      <div className="card-title">{props.title}</div>
     </div>
   </section>
 );
