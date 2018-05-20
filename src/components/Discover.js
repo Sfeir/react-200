e041c82edc18c317a1eb61d0f854b518ad20357e
Wolfgang Goedel <wgoedel@gmail.com>
2018-05-20 14:35:21 +0200

    prettier

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -5,8 +5,8 @@ import Person from './Person';
 
 // state management
 
-const succ = (current, min, max) => (current === max) ? min : current + 1;
-const pred = (current, min, max) => (current === min) ? max : current - 1;
+const succ = (current, min, max) => (current === max ? min : current + 1);
+const pred = (current, min, max) => (current === min ? max : current - 1);
 
 const setNext = ({ current }, { people }) => ({
   current: succ(current, 1, people.length)
@@ -27,19 +27,23 @@ const pause = () => ({
 // subcomponents
 
 const Fab = ({ kind, large, onClick }) => (
-  <a className={`btn-default btn-floating waves-effect waves-light ${large && 'btn-large'}`}
-     onClick={onClick}>
+  <a
+    className={`btn-default btn-floating waves-effect waves-light ${large &&
+      'btn-large'}`}
+    onClick={onClick}
+  >
     <i className="material-icons">{kind}</i>
   </a>
 );
 
-const Fabs = ({playing, next, prev, play, pause}) => (
+const Fabs = ({ playing, next, prev, play, pause }) => (
   <div className="fab-container">
     <Fab kind="skip_previous" onClick={prev} />
-    { playing
-    ? <Fab kind="pause" large onClick={pause} />
-    : <Fab kind="play_arrow" large onClick={play} />
-    }
+    {playing ? (
+      <Fab kind="pause" large onClick={pause} />
+    ) : (
+      <Fab kind="play_arrow" large onClick={play} />
+    )}
     <Fab kind="skip_next" onClick={next} />
   </div>
 );
@@ -52,13 +56,13 @@ class Discover extends Component {
     this.state = {
       current: 1,
       playing: false
-    }
+    };
   }
 
   showNextPerson = () => {
     this.setState(setNext);
   };
-  
+
   showPreviousPerson = () => {
     this.setState(setPrev);
   };
@@ -77,7 +81,7 @@ class Discover extends Component {
   componentWillUnmount() {
     clearInterval(this.intervalId);
   }
-  
+
   render() {
     const { people } = this.props;
     const { current, playing } = this.state;
@@ -85,7 +89,7 @@ class Discover extends Component {
       <div className="Discover">
         <div className="card-container">
           <Person person={people[current - 1]} />
-        </div>  
+        </div>
         <Fabs
           playing={playing}
           next={this.showNextPerson}
