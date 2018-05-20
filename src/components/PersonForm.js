81c710cf96af40f6d32d22541a346b60386be80f
Wolfgang Goedel <wgoedel@gmail.com>
2018-05-20 19:34:06 +0200

    prettier

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -4,24 +4,43 @@ import Input from './Input';
 
 class PersonForm extends Component {
   // set state to initial info from props.person
-  
+
   onSubmit = () => {
     // props.onSave with the edited info
-  }
-  
+  };
+
   render() {
     const { person, onCancel } = this.props;
     // you won't need person anymore here, but rather use state
     return (
-      <Card actions={[
-        <button type="button" className="btn btn-default" onClick={this.onSubmit} key="save">save</button>,
-        <a onClick={onCancel} key="cancel">cancel</a>
-      ]}>
-        <Card.Title
-          mainTitle={`${person.firstname} ${person.lastname}`}
+      <Card
+        actions={[
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this.onSubmit}
+            key="save"
+          >
+            save
+          </button>,
+          <a onClick={onCancel} key="cancel">
+            cancel
+          </a>
+        ]}
+      >
+        <Card.Title mainTitle={`${person.firstname} ${person.lastname}`} />
+        <Input
+          name="firstname"
+          type="text"
+          value={person.firstname}
+          label="first name"
         />
-        <Input name="firstname" type="text" value={person.firstname} label="first name" />
-        <Input name="lastname" type="text" value={person.lastname} label="last name" />
+        <Input
+          name="lastname"
+          type="text"
+          value={person.lastname}
+          label="last name"
+        />
         <Input name="entity" type="text" value={person.entity} label="entity" />
         <Input name="email" type="text" value={person.email} label="email" />
         <Input name="phone" type="text" value={person.phone} label="phone" />
--- src/pages/Person.js
+++ src/pages/Person.js
@@ -11,30 +11,33 @@ class Person extends Component {
   onEdit = () => this.setState({ editing: true });
   onCancel = () => this.setState({ editing: false });
 
-  onSave = (patch) => {
+  onSave = patch => {
     // call props.onSave with appropriate parameters
     // switch back to view mode if update successful
     // handle error otherwise
-  }
-  
+  };
+
   renderCardOrForm() {
     const { person } = this.props;
     const { editing } = this.state;
     if (editing) {
-      return <PersonForm person={person} onSave={this.onSave} onCancel={this.onCancel} />;
+      return (
+        <PersonForm
+          person={person}
+          onSave={this.onSave}
+          onCancel={this.onCancel}
+        />
+      );
     } else {
       return <PersonCard person={person} onEdit={this.onEdit} />;
     }
   }
-  
+
   render() {
     const { person } = this.props;
     return (
       <div className="card-container">
-        { person
-        ? this.renderCardOrForm()
-        : "not found :("
-        }
+        {person ? this.renderCardOrForm() : 'not found :('}
       </div>
     );
   }
