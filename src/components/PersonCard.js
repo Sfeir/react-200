93e80edfef9c52dd3265097b6018b7536ca12f21
Wolfgang Goedel <wgoedel@gmail.com>
2018-05-23 23:38:50 +0200

    prettier

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,12 +4,25 @@ import { Link } from 'react-router-dom';
 import Card from './Card';
 
 const PersonCard = ({ person, onEdit }) => (
-  <Card actions={ onEdit && [
-    <a onClick={onEdit} key="edit">edit</a>
-  ]}>
-    <Card.Avatar photoUrl={person.photo} altText={`photo of ${person.firstname}`} />
+  <Card
+    actions={
+      onEdit && [
+        <a onClick={onEdit} key="edit">
+          edit
+        </a>
+      ]
+    }
+  >
+    <Card.Avatar
+      photoUrl={person.photo}
+      altText={`photo of ${person.firstname}`}
+    />
     <Card.Title
-      mainTitle={<Link to={`/person/${person.id}`}>{person.firstname} {person.lastname}</Link>}
+      mainTitle={
+        <Link to={`/person/${person.id}`}>
+          {person.firstname} {person.lastname}
+        </Link>
+      }
       subTitle={person.entity}
     />
     <Card.Info icon="email">
@@ -18,12 +31,12 @@ const PersonCard = ({ person, onEdit }) => (
     <Card.Info icon="phone">
       <a href={`tel:${person.phone}`}>{person.phone}</a>
     </Card.Info>
-    { person.managerId && (
+    {person.managerId && (
       <Card.Info icon="supervisor_account" desc="manager">
         <Link to={`/person/${person.managerId}`}>{person.manager}</Link>
       </Card.Info>
     )}
-    { console.log(`rendering ${person.firstname}`) }
+    {console.log(`rendering ${person.firstname}`)}
   </Card>
 );
 
@@ -40,6 +53,6 @@ PersonCard.propTypes = {
     manager: PropTypes.string
   }),
   onEdit: PropTypes.func
-}
+};
 
 export default PersonCard;
