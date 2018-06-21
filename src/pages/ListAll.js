0f0cf3b6521f5851f218adc9c8fdbbfced15b7c3
Wolfgang Goedel <wgoedel@gmail.com>
2018-06-21 09:19:26 +0200

    separate ListAll and Container

--- src/pages/ListAll.js
+++ src/pages/ListAll.js
@@ -9,47 +9,40 @@ const filterPerson = search => {
   return person => re.test(person.firstname) || re.test(person.lastname);
 };
 
-// state management
-
-const searchChanged = value => () => ({
-  search: value || ''
-});
-
 // Component
 
-class ListAll extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: ''
-    };
-  }
+const ListAll = ({ people, search, onSearchChange }) => (
+  <Fragment>
+    <div className="card-container">
+      {people
+        .filter(filterPerson(search))
+        .map(person => <PersonCard person={person} key={person.id} />)}
+    </div>
+    <div className="control-container">
+      <SearchInput
+        id="search"
+        label="search by name"
+        value={search}
+        onChange={onSearchChange}
+      />
+    </div>
+  </Fragment>
+);
 
-  searchChanged = event => {
-    this.setState(searchChanged(event.target.value));
-  };
+const setValue = value => () => ({ value });
 
+class ListAllContainer extends Component {
+  state = { value: '' };
+  valueChanged = event => this.setState(setValue(event.target.value));
   render() {
-    const { people } = this.props;
-    const { search } = this.state;
     return (
-      <Fragment>
-        <div className="card-container">
-          {people
-            .filter(filterPerson(search))
-            .map(person => <PersonCard person={person} key={person.id} />)}
-        </div>
-        <div className="control-container">
-          <SearchInput
-            id="search"
-            label="search by name"
-            value={search}
-            onChange={this.searchChanged}
-          />
-        </div>
-      </Fragment>
+      <ListAll
+        people={this.props.people}
+        search={this.state.value}
+        onSearchChange={this.valueChanged}
+      />
     );
   }
 }
 
-export default ListAll;
+export default ListAllContainer;
