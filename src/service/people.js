53ece816e4240103ac11b752b7971446f82f3a46
Wolfgang Goedel <wgoedel@gmail.com>
2018-10-28 14:47:26 +0100

    upgade to CRA2

--- src/service/people.js
+++ src/service/people.js
@@ -1,10 +1,10 @@
 export async function fetchPeople() {
-  const response = await fetch('/api/people');
+  const response = await fetch('//localhost:3001/people');
   return await response.json();
 }
 
 export async function updatePerson(id, patch) {
-  const response = await fetch(`/api/people/${id}`, {
+  const response = await fetch(`//localhost:3001/people/${id}`, {
     method: 'PATCH',
     body: JSON.stringify(patch),
     headers: { 'Content-Type': 'application/json' }
