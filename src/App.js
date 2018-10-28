c25bca95826276cf87255b7fba938530481649ce
Wolfgang Goedel <wgoedel@gmail.com>
2018-10-28 20:08:20 +0100

    remove some classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
@@ -8,35 +8,30 @@ import Person from './pages/Person.container';
 import AppBar from './components/AppBar';
 import Spinner from './components/Spinner';
 
-class App extends Component {
-  componentDidMount() {
-    this.props
-      .loadPeople()
-      .then(success => !success && alert('could not load people :('));
-  }
+const App = ({ loadPeople, peopleLoading }) => {
+  useEffect(() => {
+    loadPeople().then(success => !success && alert('could not load people :('));
+  }, []);
 
-  render() {
-    const { peopleLoading } = this.props;
-    return (
-      <div className="App">
-        <header>
-          <AppBar />
-        </header>
-        <main>
-          {peopleLoading ? (
-            <Spinner />
-          ) : (
-            <Switch>
-              <Route path="/all" component={ListAll} />
-              <Route path="/discover" component={Discover} />
-              <Route path="/person/:id" component={Person} />
-              <Redirect to="/all" />
-            </Switch>
-          )}
-        </main>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header>
+        <AppBar />
+      </header>
+      <main>
+        {peopleLoading ? (
+          <Spinner />
+        ) : (
+          <Switch>
+            <Route path="/all" component={ListAll} />
+            <Route path="/discover" component={Discover} />
+            <Route path="/person/:id" component={Person} />
+            <Redirect to="/all" />
+          </Switch>
+        )}
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import PersonCard from '../components/PersonCard';
 import PersonForm from '../components/PersonForm';
 
-class Person extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { editing: false };
-  }
+const Person = ({ id, personExists }) => {
+  const [editing, setEditing] = useState(false);
 
-  beginEdit = () => this.setState({ editing: true });
-  endEdit = () => this.setState({ editing: false });
+  const beginEdit = useCallback(() => setEditing(true));
+  const endEdit = useCallback(() => setEditing(false));
 
-  renderCardOrForm() {
-    const { id } = this.props;
-    const { editing } = this.state;
+  const renderCardOrForm = () => {
     if (editing) {
-      return <PersonForm id={id} onDone={this.endEdit} />;
+      return <PersonForm id={id} onDone={endEdit} />;
     } else {
-      return <PersonCard id={id} onEdit={this.beginEdit} />;
+      return <PersonCard id={id} onEdit={beginEdit} />;
     }
-  }
+  };
 
-  render() {
-    const { personExists } = this.props;
-    return (
-      <div className="card-container">
-        {personExists ? this.renderCardOrForm() : 'not found :('}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-container">
+      {personExists ? renderCardOrForm() : 'not found :('}
+    </div>
+  );
+};
 
 export default Person;
